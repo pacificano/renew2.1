5ab8530bf3576b1b07e0da3ed8ae25ca1654880d
pacificano <tris.pacificano@gmail.com>
2015-10-05 12:26:27 +1100

    Wordpress theme

---
+++ Wordpress/wp-content/themes/pacificano/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Pacificano_v1
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pacificano' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'pacificano' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'pacificano' ), 'pacificano', '<a href="http://www.pacificano.com" rel="designer">Pacificano</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
