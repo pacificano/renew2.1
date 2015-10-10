acec7a8b8cc693c71397b02c597581696b49033d
pacificano <tris.pacificano@gmail.com>
2015-10-10 18:00:43 +1100

    customised tinyMCE

--- Wordpress/wp-content/themes/pacificano/functions.php
+++ Wordpress/wp-content/themes/pacificano/functions.php
@@ -150,3 +150,38 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter('mce_buttons_2', 'my_mce_buttons_2');
+
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {  
+	// Define the style_formats array
+	$style_formats = array(  
+		// Each array child is a format with it's own settings
+		array(  
+			'title' => 'Pacificano Button',
+			'classes' => 'btn btn-pacificano',
+			'selector' => 'a',
+		),
+		array(  
+			'title' => '.p lead',
+			'classes' => 'lead',
+			'selector' => 'p',
+		),
+	);  
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );  
+	
+	return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );  
