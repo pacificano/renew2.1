e1aa47b34ea68cda345465520f80f4d4e801552d
pacificano <tris.pacificano@gmail.com>
2015-10-10 14:09:27 +1100

    Template Part: Services

---
+++ Wordpress/wp-content/themes/pacificano/content-services.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Custom Fields
+$about_us_body	= get_field('about_us_body');
+
+?>
+
+<!-- OUR SERVICES -->
+<section id="our-services">
+
+	<div class="container aboutDiv">
+	
+    	<div class="row">
+    		
+    		<div class="col-md-12">
+    			
+    			<h2>Our Services</h2>
+
+				<p class="lead"><?php echo $about_us_body; ?></p>
+
+    		</div>
+
+    	</div>
+
+    </div>
+	
+</section>
