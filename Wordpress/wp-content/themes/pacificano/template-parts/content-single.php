883c345a7b82923651eff422a59c85ea28988b35
pacificano <tris.pacificano@gmail.com>
2015-10-12 10:31:01 +1100

    styled the content of a blog post

--- Wordpress/wp-content/themes/pacificano/template-parts/content-single.php
+++ Wordpress/wp-content/themes/pacificano/template-parts/content-single.php
@@ -11,25 +11,38 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
-		<div class="entry-meta">
-			<?php pacificano_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		<?php if ( 'post' === get_post_type() ) : ?>
+		
+			<div class="post-details">
+                <i class="fa fa-user"></i> <?php the_author(); ?>
+                <i class="fa fa-clock-o"></i> <?php the_date(); ?>
+                <i class="fa fa-folder-open"></i> <?php the_category(', '); ?>
+				<?php the_tags('<i class="fa fa-tags"></i> ', ', ', ''); ?>
+
+                <?php edit_post_link(' Edit', '<i class="fa fa-pencil"></i>', ''); ?>
+
+                <div class="post-comments-badge">
+                    <a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+                </div>
+
+            </div><!-- post-details -->
+
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pacificano' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	<?php if (has_post_thumbnail() ) { // check for feature image ?>
+	
+		<div class="post-image">
+	        <?php the_post_thumbnail(); ?>
+	    </div>
 
-	<footer class="entry-footer">
-		<?php pacificano_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <?php } ?>
+
+    <div class="post-body">
+        <?php the_content(); ?>
+    </div>
+
 </article><!-- #post-## -->
 
