88184723fbbc080f74cfdd955f1902053af5a1e0
pacificano <tris.pacificano@gmail.com>
2015-10-12 10:30:42 +1100

    styled the search page

--- Wordpress/wp-content/themes/pacificano/template-parts/content-search.php
+++ Wordpress/wp-content/themes/pacificano/template-parts/content-search.php
@@ -11,21 +11,37 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php pacificano_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		
+			<div class="post-details">
+                <i class="fa fa-user"></i> <?php the_author(); ?>
+                <i class="fa fa-clock-o"></i> <?php the_date(); ?>
+                <i class="fa fa-folder-open"></i> <?php the_category(', '); ?>
+				<?php the_tags('<i class="fa fa-tags"></i> ', ', ', ''); ?>
+
+                <?php edit_post_link(' Edit', '<i class="fa fa-pencil"></i>', ''); ?>
+
+                <div class="post-comments-badge">
+                    <a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+                </div>
+
+            </div><!-- post-details -->
+
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	<?php if (has_post_thumbnail() ) { // check for feature image ?>
+	
+		<div class="post-image">
+	        <?php the_post_thumbnail(); ?>
+	    </div>
 
-	<footer class="entry-footer">
-		<?php pacificano_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <?php } ?>
+
+    <div class="post-excerpt">
+        <?php the_excerpt(); ?>
+    </div>
+
 </article><!-- #post-## -->
-
