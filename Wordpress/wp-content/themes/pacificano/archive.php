635a1808b4ac184753be140d5e2c723718ebfc36
pacificano <tris.pacificano@gmail.com>
2015-10-12 10:29:37 +1100

    the archive page

--- Wordpress/wp-content/themes/pacificano/archive.php
+++ Wordpress/wp-content/themes/pacificano/archive.php
@@ -9,43 +9,62 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+		<!-- FEATURE IMAGE -->
+		<section class="feature-image feature-image-default-alt">
+		    <div class="container clearfix">
+		        <div class="row">
+		            
+		            <div class="col-md-8 white">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+		                <?php
+							the_archive_title( '<h1 class="dropShadow">', '' );
+							the_archive_description( '<small class="taxonomy-description">', '</small>' );
+							echo "</h1>";
+						?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		            </div>
 
-				<?php
+		        </div>
+		    </div>
+		</section>
+		
+		<div class="container">
+			<div id="primary" class="row">
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+				<main id="content" class="col-sm-9">
 
-			<?php endwhile; ?>
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php the_posts_navigation(); ?>
+						<?php
 
-		<?php else : ?>
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_format() );
+						?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php endwhile; ?>
 
-		<?php endif; ?>
+					<?php the_posts_navigation(); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					<?php else : ?>
 
-<?php get_sidebar(); ?>
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+					<?php endif; ?>
+
+				</main><!-- main -->
+
+				<aside class="col-sm-3">
+					<?php get_sidebar(); ?>
+				</aside><!-- aside -->
+
+			</div><!-- row -->
+		</div><!-- container -->
+
 <?php get_footer(); ?>
