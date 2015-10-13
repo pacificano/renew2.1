e211ee2123d0fd755f2112825eb5a8ef8bdd281b
pacificano <tris.pacificano@gmail.com>
2015-10-13 21:35:57 +1100

    removed extra sidebar

--- Wordpress/wp-content/themes/pacificano/functions.php
+++ Wordpress/wp-content/themes/pacificano/functions.php
@@ -107,16 +107,6 @@ function pacificano_widgets_init() {
 		'before_title'  => '<h4 class="widget-title">',
 		'after_title'   => '</h4>',
 	) );
-
-	register_sidebar( array(
-		'name'          => esc_html__( 'Footer Sidebar', 'pacificano' ),
-		'id'            => 'sidebar-2',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h4 class="widget-title">',
-		'after_title'   => '</h4>',
-	) );
 }
 add_action( 'widgets_init', 'pacificano_widgets_init' );
 
