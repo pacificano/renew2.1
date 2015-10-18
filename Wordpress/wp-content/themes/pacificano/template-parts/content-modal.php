7c29662c9417a8f2a0892703139fb0f3001b9eb2
pacificano <tris.pacificano@gmail.com>
2015-10-18 15:32:29 +1100

    the newsletter modal

---
+++ Wordpress/wp-content/themes/pacificano/template-parts/content-modal.php
@@ -0,0 +1,53 @@
+<!-- modal -->
+<div class="modal fade" id="myModal">
+    
+    <div class="modal-dialog">
+        
+        <div class="modal-content">
+            
+            <div class="modal-header">
+                
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+                </button>
+
+                <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our mailing list</h4>
+                
+
+            </div><!-- modal header -->
+
+            <div class="modal-body">
+                
+                <p>Simply enter your name and email!</em></p>
+
+                <form action="http://pacificano.us11.list-manage.com/subscribe/post?u=9e3e430d9b1934531b447d0aa&amp;id=5658fa2591" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div class="form-group">
+                        <label class="sr-only" for="mce-FNAME">Your first name</label>
+                        <input type="text" value="" name="FNAME" class="newsletterName" id="mce-FNAME" placeholder="Your first name">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="mce-EMAIL">and your email</label>
+                        <input type="email" value="" name="EMAIL" class="required email newsletterEmail" id="mce-EMAIL" placeholder="Your email">
+                    </div>
+
+                    <div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none; margin-bottom: 20px; border: solid 2px #ddd; padding: 10px;"></div>
+						<div class="response" id="mce-success-response" style="display:none; margin-bottom: 20px; border: solid 2px #ddd; padding: 10px;">Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.</div>
+					</div> 
+
+					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9e3e430d9b1934531b447d0aa_5658fa2591" tabindex="-1" value=""></div>
+
+                    <input type="submit" value="Subscribe!" name="subscribe" id="mc-embedded-subscribe" class="button newsletterButton btn btn-pacificano">
+                </form>
+
+                <hr style="padding-bottom: 0px; margin-bottom: 10px;">
+
+                <p><small>By providing your email you consent to receiving occassional promotional emails &amp; newsletters.<br>No spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+
+            </div><!-- modal body -->
+
+        </div><!-- modal content -->
+    </div><!-- modal dialog -->
+
+</div><!-- modal -->
